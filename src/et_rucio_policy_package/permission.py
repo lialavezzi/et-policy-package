19dd6479118d5340837a9139a0136e652c42dfec
Lia Lavezzi <lia.lavezzi@to.infn.it>
2026-02-22 21:33:38 +0100

    test 2

--- src/et_rucio_policy_package/permission.py
+++ src/et_rucio_policy_package/permission.py
@@ -490,16 +490,13 @@ def perm_update_rule(issuer: "InternalAccount", kwargs: dict[str, Any], session:
     :returns: True if account is allowed to call the API call, otherwise False
     """
 
-    print("**** permission ",issuer, _is_root(issuer),"*************")
-    
     if _is_root(issuer) or has_account_attribute(account=issuer, key='admin', session=session):
         return True
 
-    # Check the accounts of the issued rules                                                                                 >
+    # Check the accounts of the issued rules
     for rule in kwargs.get('rules', []):
-      print("**** permission ",issuer, _is_root(issuer), rule['account'])
-      if rule['account'] == issuer and has_account_attribute(account=issuer, key='can_upload', session=session):
-        return True
+        if rule['account'] == issuer and has_account_attribute(account=issuer, key='can_upload', session=session):
+            return True
 
     return False
 
